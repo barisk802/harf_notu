bb9eac2f406e54040c87e951a7d94f41fa9a93df
barisk802 <barisk802@gmail.com>
2025-10-18 05:20:05 +0300

    harf_notu.py

--- harf_notu.py
+++ harf_notu.py
@@ -2,16 +2,15 @@ import streamlit as st
 
 st.title("📘 Harf Notu Hesaplama Sistemi")
 
-st.write("Vize 1, Vize 2 ve Final notunuzu giriniz:")
+st.write("Vize 1 ve Final notunuzu giriniz:")
 
 # Kullanıcıdan girişleri al
 vize1 = st.number_input("Vize 1:", min_value=0, max_value=100, step=1)
-vize2 = st.number_input("Vize 2:", min_value=0, max_value=100, step=1)
 final = st.number_input("Final:", min_value=0, max_value=100, step=1)
 
 # Hesapla butonu
 if st.button("Hesapla"):
-    genel_not = vize1 * 3/10 + vize2 * 3/10 + final * 7/10
+    genel_not = vize1 * 3/10 + final * 7/10
     st.write(f"Genel Notunuz: **{genel_not:.2f}**")
 
     # Harf notu koşulları
@@ -33,3 +32,4 @@ if st.button("Hesapla"):
         st.warning("Harf Notunuz: **FD**")
     else:
         st.error("Harf Notunuz: **FF ❌**")
+
